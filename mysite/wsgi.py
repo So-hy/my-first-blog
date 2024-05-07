f723b2bfc73e282c7e29b05254c376df6f473703
So-hy <sohyun3110@gmail.com>
2024-05-07 17:15:31 +0900

    Changed the HTML for the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+path = '/home/sohy3110/my-first-blog'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] =  'mysite.settings'
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
