deb1ecdc8aeedd14e712dabffd83a6e13f055ac4
So-hy <sohyun3110@gmail.com>
2024-06-17 22:44:43 +0900

    change

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,6 +16,10 @@ path = '/home/sohy3110/my-first-blog'
 if path not in sys.path:
     sys.path.append(path)
 
+activate_env = os.path.expanduser('/home/sohy3110/my-first-blog/myvenv/bin/activate_this.py')
+with open(activate_env) as f:
+    exec(f.read(), dict(__file__=activate_env))
+
 os.environ['DJANGO_SETTINGS_MODULE'] =  'mysite.settings'
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
